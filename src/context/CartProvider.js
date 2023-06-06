d8d21bcf40f67cbdfdd7ee1618242dc0b04120d7
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-06-06 17:19:07 -0500

    added cart reducer

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cart-context'
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+// useReducer handles more complex state, outside of component function it doesn't need to be recreated everytime and needs nothing from the component below
+const cartReducer = (state, action) => {
+  // last state snapshot of the state managed by the reducer
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item)
+    // making a new array for state items, rendering brand new state object
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
+    // must return a new state snapshot
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return defaultCartState
+}
+
 const CartProvider = props => {
-  const addItemToCart = () => {}
-  const removeItemFromCart = () => {}
+  // state element, function are  destructured
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
+  const addItemToCart = item => {
+    dispatchCartAction({ type: 'ADD', item: item })
+    // property names to identify the action name it something that makes sense i.e type
+    // forward item you expect to get to the reducer as second argument i.e item
+  }
+  const removeItemFromCart = id => {
+    dispatchCartAction({ type: 'REMOVE', id: id })
+  }
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCart,
     removeItem: removeItemFromCart
   }
@@ -18,3 +51,5 @@ const CartProvider = props => {
 }
 
 export default CartProvider
+
+
